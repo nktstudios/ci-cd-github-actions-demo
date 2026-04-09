0d7dd4dff93a7583ed369155e6f56c8f72ea278b
Ndafara Tsamba <ntsamba@gmail.com>
2026-04-09 02:23:57 +0200

    chore: fix errors and update message

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {
-        "message": "Hello Github Actions Demo from version 1!"
+        "message": "Hello Github Actions Demo from version 2!"
         }
 
 @app.get("/sum/{a}/{b}")
 def sum_numbers(a: int, b: int):
     return {
-        "result": a + b + b
+        "result": a + b
         }
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello Github Actions Demo from version 1!"}
+    assert response.json() == {"message": "Hello Github Actions Demo from version 2!"}
 
 def test_sum():
     response = client.get("/sum/2/3")
